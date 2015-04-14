170567b6eab75c39c0693d53058277c51cd0d8ce
hoodq <hoodq@colorado.edu>
2015-04-13 21:44:18 -0600

    ace can now be 11 or 1 depending on the situation and card value code refactored into a method, making card_value hash an instance variable

--- black_jack_game.rb
+++ black_jack_game.rb
@@ -1,25 +1,26 @@
 require_relative 'deck'
 
 deck1 = Deck.new
-deck1.shuffle
 
 dealer_hand = Array.new
 player_hand = Array.new
 
-card_value = Hash.new
-card_value[2] = 2
-card_value[3] = 3
-card_value[4] = 4
-card_value[5] = 5
-card_value[6] = 6
-card_value[7] = 7
-card_value[8] = 8
-card_value[9] = 9
-card_value[10] = 10
-card_value[:jack] = 10
-card_value[:queen] = 10
-card_value[:king] = 10
-card_value[:ace] = [11, 1]
+@card_value = Hash.new
+@card_value[2] = 2
+@card_value[3] = 3
+@card_value[4] = 4
+@card_value[5] = 5
+@card_value[6] = 6
+@card_value[7] = 7
+@card_value[8] = 8
+@card_value[9] = 9
+@card_value[10] = 10
+@card_value[:jack] = 10
+@card_value[:queen] = 10
+@card_value[:king] = 10
+@card_value[:ace] = 11
+
+deck1.shuffle
 
 deck1.draw_card(player_hand)
 
@@ -33,42 +34,51 @@ deck1.draw_card(dealer_hand)
 puts "\ndealer's first card is: #{dealer_hand[0]}"
 sleep(3)
 
+def value_hand hand
+  value = 0
+  hand.each do |card|
+    if card[0] == :ace
+      value += @card_value[card[0]]
+      if value > 21
+        value -= 10
+      end  
+    else
+      value += @card_value[card[0]]    
+    end
+  end
+  value
+end
+
+player_value = 0
+
 loop do
-  puts "\nwould you like to hit or stay?"
+  player_value = value_hand(player_hand)
+  if player_value > 21
+    puts "\nThe value of your hand is #{player_value}. You busted."
+    break
+  end
+  puts "\nWould you like to hit or stay?"
   answer = gets.chomp.downcase
 	if answer == 'hit'
-  	  deck1.draw_card(player_hand)
-  	  puts "Player's hand is: #{player_hand}"
-      sleep(3)
-  	elsif answer == 'stay'
-  	  puts "\nplayer's hand is: #{player_hand}"
-  	  puts "\ndealer's hand is: #{dealer_hand}"
-  	  break
+ 	  deck1.draw_card(player_hand)
+ 	  puts "Player's hand is: #{player_hand}"
+    sleep(3)
+  elsif answer == 'stay'
+	  break
 	end
 end
 
 dealer_value = 0
 
-dealer_hand.each do |card|
-  if card[0] == :ace && dealer_value > 21
-    
-  end
-  dealer_value += card_value[card[0]]
-end
+dealer_value = value_hand(dealer_hand)
 
-player_value = 0
-
-player_hand.each do |card|
-  player_value += card_value[card[0]]
-end
+player_value = value_hand(player_hand)
 
 while dealer_value < 17
   dealer_value = 0
   puts "\nDealer must hit"
   deck1.draw_card(dealer_hand)
-  dealer_hand.each do |card|
-    dealer_value += card_value[card[0]]
-  end
+  dealer_value = value_hand(dealer_hand)
 end
 
 puts "\nDealer's hand is: #{dealer_hand}"
